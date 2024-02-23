2def40557a8e23843ec27fa334113c46bb361ed3
sylligardos <sylli1998@gmail.com>
2024-02-23 16:31:36 +0100

    Adding scripts to train all rockets

---
+++ train_rockets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 train_rocket.py --path=data/TSB_16/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_16.csv --eval-true --path_save=results/weights/supervised/
+python3 train_rocket.py --path=data/TSB_32/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_32.csv --eval-true --path_save=results/weights/supervised/
+python3 train_rocket.py --path=data/TSB_64/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_64.csv --eval-true --path_save=results/weights/supervised/
+python3 train_rocket.py --path=data/TSB_128/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_128.csv --eval-true --path_save=results/weights/supervised/
+python3 train_rocket.py --path=data/TSB_256/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_256.csv --eval-true --path_save=results/weights/supervised/
+python3 train_rocket.py --path=data/TSB_512/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_512.csv --eval-true --path_save=results/weights/supervised/
+python3 train_rocket.py --path=data/TSB_768/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_768.csv --eval-true --path_save=results/weights/supervised/
+python3 train_rocket.py --path=data/TSB_1024/ --split_per=0.7 --file=experiments/supervised_splits/split_TSB_1024.csv --eval-true --path_save=results/weights/supervised/
